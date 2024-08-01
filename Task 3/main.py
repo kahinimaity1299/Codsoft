9ad49927868087a73531217c1037a2b7c77aeced
KAHINI MAITY <162483500+kahinimaity1299@users.noreply.github.com>
2024-08-01 11:57:20 +0530

    Update main.py

--- Task 3/main.py
+++ Task 3/main.py
@@ -1,1 +1,64 @@
 
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+import statsmodels.formula.api as sm
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+import warnings
+
+df = pd.read_csv("Advertising.csv")
+
+df.head()
+
+df.columns
+
+df.rename(columns={'Unnamed: 0': 'Index'}, inplace=True)
+
+df.shape
+
+df.info()
+
+sns.pairplot(df, x_vars=["TV", "Radio", "Newspaper"], y_vars="Sales", kind="reg")
+
+# Model Preparation
+
+X = df.drop('Sales', axis=1)
+y = df[["Sales"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=46)
+
+lin_model = sm.ols(formula="Sales ~ TV + Radio + Newspaper", data=df).fit()
+
+print(lin_model.params, "\n")
+
+print(lin_model.summary())
+
+results = []
+names = []
+
+models = [('LinearRegression', LinearRegression())]
+
+for name, model in models:
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    result = np.sqrt(mean_squared_error(y_test, y_pred))
+    results.append(result)
+    names.append(name)
+    msg = "%s: %f" % (name, result)
+    print(msg)
+
+new_data = pd.DataFrame({'TV': [100], 'Radio': [50], 'Newspaper': [25]})
+predicted_sales = lin_model.predict(new_data)
+print("Predicted Sales:", predicted_sales)
+
+ew_data = pd.DataFrame({'TV': [25], 'Radio': [63], 'Newspaper': [80]})
+predicted_sales = lin_model.predict(new_data)
+print("Predicted Sales:", predicted_sales)
+
