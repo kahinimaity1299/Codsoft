aae8bca13efac59bcd088f0e5c009457b6ee16d1
KAHINI MAITY <162483500+kahinimaity1299@users.noreply.github.com>
2024-08-01 11:50:22 +0530

    Update main.py

--- Task 2/main.py
+++ Task 2/main.py
@@ -1,1 +1,74 @@
 
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('movies_data.csv')
+df.head()
+df.info()
+
+from sklearn.model_selection import train_test_split
+
+Input = df.drop(['Name', 'Genre', 'Rating', 'Director', 'Actor 1', 'Actor 2', 'Actor 3'], axis=1)
+Output = df['Rating']
+
+x_train, x_test, y_train, y_test = train_test_split(Input, Output, test_size = 0.2, random_state = 1)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score as score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+
+def evaluate_model(y_true, y_pred, model_name):
+    print("Model: ", model_name)
+    print("Accuracy = {:0.2f}%".format(score(y_true, y_pred)*100))
+    print("Mean Squared Error = {:0.2f}\n".format(mean_squared_error(y_true, y_pred, squared=False)))
+    return round(score(y_true, y_pred)*100, 2)
+
+LR = LinearRegression()
+LR.fit(x_train, y_train)
+lr_preds = LR.predict(x_test)
+
+RFR = RandomForestRegressor(n_estimators=100, random_state=1)
+RFR.fit(x_train, y_train)
+rf_preds = RFR.predict(x_test)
+
+DTR = DecisionTreeRegressor(random_state=1)
+DTR.fit(x_train, y_train)
+dt_preds = DTR.predict(x_test)
+
+XGBR = XGBRegressor(n_estimators=100, random_state=1)
+XGBR.fit(x_train, y_train)
+xgb_preds = XGBR.predict(x_test)
+
+GBR = GradientBoostingRegressor(n_estimators=100, random_state=60)
+GBR.fit(x_train, y_train)
+gb_preds = GBR.predict(x_test)
+
+LGBMR = LGBMRegressor(n_estimators=100, random_state=60)
+LGBMR.fit(x_train, y_train)
+lgbm_preds = LGBMR.predict(x_test)
+
+KNR = KNeighborsRegressor(n_neighbors=5)
+KNR.fit(x_train, y_train)
+knn_preds = KNR.predict(x_test)
+
+LRScore = evaluate_model(y_test, lr_preds, "LINEAR REGRESSION")
+RFScore = evaluate_model(y_test, rf_preds, "RANDOM FOREST")
+DTScore = evaluate_model(y_test, dt_preds, "DECEISION TREE")
+XGBScore = evaluate_model(y_test, xgb_preds, "EXTENDED GRADIENT BOOSTING")
+GBScore = evaluate_model(y_test, gb_preds, "GRADIENT BOOSTING")
+LGBScore = evaluate_model(y_test, lgbm_preds, "LIGHT GRADIENT BOOSTING")
+KNNScore = evaluate_model(y_test, knn_preds, "K NEAREST NEIGHBORS")
+
+models = pd.DataFrame(
+    {
+        "MODELS": ["Linear Regression", "Random Forest", "Decision Tree", "Gradient Boosting", "Extended Gradient Boosting", "Light Gradient Boosting","K Nearest Neighbors"],
+        "SCORES": [LRScore, RFScore, DTScore, GBScore, XGBScore, LGBScore, KNNScore]
+    }
+)
+models.sort_values(by='SCORES', ascending=False)
+
